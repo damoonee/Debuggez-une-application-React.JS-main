d39f1050a2be304cf70754f6848c115e0f3e952f
Manon <manonmarchaufromy@gmail.com>
2024-01-10 14:39:47 +0100

    modification du slide et des filtres

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -6,29 +6,29 @@ import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);  // État pour suivre l'index de la slide actuel
+  
+  // Triez les données par date dans l'ordre décroissant
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+
+  // Intervalle de 5 secondes pour changer l'image automatiquement
   useEffect(() => {
-    nextCard();
-  });
+    const interval = setInterval(() => {
+      setIndex(prevIndex => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
+    }, 5000);
+    
+    // Remet à zero l'interval au changement d'image qu'il soit automatique ou manuel
+    return () => clearInterval(interval);  
+  }, [byDateDesc]);
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+        <div key={event.id}>
+          {/* Affiche la diapositive seulement si son index correspond à l'index actuel */}
+          <div className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -38,20 +38,20 @@ const Slider = () => {
               </div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              key={`${event.id}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
